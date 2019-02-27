6d8d5702cbff1a4b7a842c46edaaf1a53a34d654
kyleschaeffer <kyle@spirito.us>
2019-02-26 22:03:47 -0500

    Add fuse.js fuzzy searches, hash state handling, and style updates

--- js/app.js
+++ js/app.js
@@ -1,4 +1,21 @@
 const axios = require('axios');
+const Fuse = require('fuse.js');
+const queryString = require('query-string');
+
+// Fuse config
+const fuseOptions = {
+  shouldSort: true,
+  threshold: 0.2,
+  location: 0,
+  distance: 100,
+  maxPatternLength: 32,
+  minMatchCharLength: 1,
+  keys: [
+    'name',
+    'entity',
+    'hex',
+  ],
+};
 
 const init = async function () {
   // Get elements
@@ -6,12 +23,13 @@ const init = async function () {
   const glyphResults = document.getElementById('glyphs');
 
   // Get glyph data
-  const glyphs = {};
+  let glyphs = [];
+  let fuse;
   try {
     const utf = await axios.get('glyphs/utf.json');
     const emoji = await axios.get('glyphs/emoji.json');
-    glyphs.utf = utf.data;
-    glyphs.emoji = emoji.data;
+    glyphs = utf.data.chars.concat(emoji.data.chars);
+    fuse = new Fuse(glyphs, fuseOptions);
   } catch (e) {
     console.error(e);
   }
@@ -21,8 +39,64 @@ const init = async function () {
   searchBox.setAttribute('placeholder', 'Search for glyphs\u2026');
   searchBox.focus();
 
-  // Track current search keywords
-  let keywords;
+  // Application state
+  const state = {
+    k: '',
+    c: null,
+    max: 256,
+    last: null,
+  };
+
+  // Generate state hash
+  const stateHash = state => {
+    return `#k=${encodeURIComponent(state.k)}${state.c ? `&c=${encodeURIComponent(state.c)}` : ''}`;
+  };
+
+  // Perform search
+  const searchGlyphs = () => {
+    // Search hasn't changed
+    if (state.k === state.last) return;
+
+    // Reset glyphs
+    glyphResults.innerHTML = '';
+
+    // No search terms
+    if (!state.k) return;
+
+    // HTML builder
+    let html = '';
+
+    // Search glyphs
+    const results = fuse.search(state.k);
+    for (let i = 0; i < results.length; i++) {
+      html += `<li title="${results[i].name}" data-char="${results[i].hex}">${charHexes(results[i])}</li>`;
+      if (i >= state.max) break;
+    };
+
+    // Update glyph results
+    glyphResults.innerHTML = html;
+
+    // Save last search
+    state.last = state.k;
+  };
+
+  // Handle hash changes
+  const hashChange = () => {
+    // Update state
+    const { k, c } = queryString.parse(location.hash);
+    state.k = k ? k : '';
+    state.c = c ? c : null;
+
+    // Update state hash
+    const newStateHash = stateHash(state);
+    if (location.hash != newStateHash) location.replace(newStateHash);
+
+    // Update search box
+    searchBox.value = state.k;
+
+    // Trigger a new search
+    searchGlyphs();
+  };
 
   // Get hexes from character
   const charHexes = char => {
@@ -34,54 +108,24 @@ const init = async function () {
     return hexes;
   };
 
-  // Perform search
-  const searchGlyphs = e => {
-    // Search hasn't changed
-    if (e.target.value === keywords) return;
-
-    // Update keywords
-    keywords = e.target.value;
-
-    // Reset glyphs
-    glyphResults.innerHTML = '';
-
-    // No search terms
-    if (!keywords) return;
-
-    // Matching pattern
-    let pattern = new RegExp(`${keywords}`, 'ig');
-    if (keywords.length === 1) pattern = new RegExp(` ${keywords}$`, 'ig');
-
-    // HTML builder
-    let html = '';
-    let resultCount = 0;
-
-    // Search glyphs
-    glyphs.utf.chars.forEach(char => {
-      if (char.name && char.name.match(pattern)) {
-        html += `<li title="${char.name}">${charHexes(char)}</li>`;
-        resultCount++;
-      }
-      if (resultCount >= 256) return false;
-    });
-
-    // Search Emoji
-    glyphs.emoji.chars.forEach(char => {
-      if (char.name && char.name.match(pattern)) {
-        html += `<li title="${char.name}">${charHexes(char)}</li>`;
-        resultCount++;
-      }
-      if (resultCount >= 256) return false;
-    });
-
-    // Update glyph results
-    glyphResults.innerHTML = html;
+  // Handle input changes
+  const inputChange = e => {
+    location.replace(stateHash({
+      k: e.target.value,
+      c: state.char,
+    }));
   };
 
   // Search
-  searchBox.addEventListener('keyup', searchGlyphs);
-  searchBox.addEventListener('change', searchGlyphs);
-  searchBox.addEventListener('search', searchGlyphs);
+  searchBox.addEventListener('keyup', inputChange);
+  searchBox.addEventListener('change', inputChange);
+  searchBox.addEventListener('search', inputChange);
+
+  // Hash
+  window.addEventListener('hashchange', hashChange);
+
+  // Load state
+  hashChange();
 };
 
 init();
