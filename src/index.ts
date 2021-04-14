01497a4e2308a63a7e586f2ab0b7e8940ed851f5
Kyle Schaeffer <kyle@facet.ai>
2021-04-14 11:39:36 -0400

    Fix build, tweak styles

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { UNICODE_VERSION } from './config/unicode'
 
 const app = document.getElementById('app')
 if (app) {
-  app.innerHTML = `Unicode: ${UNICODE_VERSION}`
+  app.innerHTML = `Unicode: <a href="#">${UNICODE_VERSION}</a>`
 }
 
 console.log('Hi loaded')
