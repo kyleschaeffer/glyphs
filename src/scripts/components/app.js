46925025a91522e2b8d044a929262d6f10c79c82
Kyle Schaeffer <kyle@facet.ai>
2022-02-21 10:36:52 -0500

    Add rendering utils

--- src/scripts/components/app.js
+++ src/scripts/components/app.js
@@ -1,6 +1,6 @@
 'use strict'
 
-import * as config from '../config.js'
+import { UNICODE_VERSION } from '../config.js'
 import { GlyphsComponent } from './component.js'
 
 /**
@@ -15,27 +15,42 @@ import { GlyphsComponent } from './component.js'
 customElements.define(
   'glyphs-app',
   class extends GlyphsComponent {
-    onMount() {
-      this.state = {
-        /**
-         * @type {Map<string, Glyph>} Unicode character data
-         */
+    constructor() {
+      super({
         glyphs: new Map(),
-
-        /**
-         * @type {boolean} Loading state
-         */
         loading: false,
-      }
+      })
+    }
 
+    template() {
+      return `
+        <div class="app">
+          <h1>Glyphs</h1>
+          <div class="loading" -if="loading">Loading&hellip;</div>
+          <div class="content" -ifnot="loading">
+            <slot name="content"></slot>
+          </div>
+        </div>
+      `
+    }
+
+    style() {
+      return `
+        h1 {
+          margin-block: 0;
+        }
+      `
+    }
+
+    onMount() {
       this.loadGlyphData()
     }
 
     async loadGlyphData() {
-      this.state.loading = true
-      const data = await fetch(`../glyphs/${config.UNICODE_VERSION}.json`).then((res) => res.json())
+      this.setState('loading', true)
+      const data = await fetch(`../glyphs/${UNICODE_VERSION}.json`).then((res) => res.json())
       this.state.glyphs = new Map(data)
-      this.state.loading = false
+      this.setState('loading', false)
       console.log(this.state)
     }
   }
--- src/scripts/components/component.js
+++ src/scripts/components/component.js
@@ -1,20 +1,57 @@
 export class GlyphsComponent extends HTMLElement {
-  constructor() {
+  constructor(initialState = {}) {
     super()
+    this.state = initialState
 
-    this.bindTemplate()
+    this._bindTemplate()
     this.onMount()
+    this.render()
   }
 
-  bindTemplate() {
+  template() {
+    return ''
+  }
+
+  style() {
+    return ''
+  }
+
+  _bindTemplate() {
     const templateId = `${this.tagName.toLowerCase()}-template`
-    const template = document.getElementById(templateId)
+    let template = document.getElementById(templateId)
     if (!template) {
-      throw new Error(`Template not found: #${templateId}`)
+      template = document.createElement('template')
+      template.id = templateId
+      template.innerHTML = this.template()
+      document.body.appendChild(template)
+
+      const sheet = new CSSStyleSheet()
+      sheet.replaceSync(this.style())
+      document.adoptedStyleSheets = [...document.adoptedStyleSheets, sheet]
     }
 
     this.attachShadow({ mode: 'open' }).appendChild(template.content.cloneNode(true))
+    this.shadowRoot.adoptedStyleSheets = document.adoptedStyleSheets
   }
 
   onMount() {}
+
+  setState(prop, value) {
+    this.state = { ...this.state, [prop]: value }
+    this.render()
+  }
+
+  render() {
+    for (const el of this.shadowRoot.querySelectorAll('[-if]')) {
+      const prop = el.getAttribute('-if')
+      if (prop && this.state[prop]) el.style.display = 'block'
+      else el.style.display = 'none'
+    }
+
+    for (const el of this.shadowRoot.querySelectorAll('[-ifnot]')) {
+      const prop = el.getAttribute('-ifnot')
+      if (prop && !this.state[prop]) el.style.display = 'block'
+      else el.style.display = 'none'
+    }
+  }
 }
