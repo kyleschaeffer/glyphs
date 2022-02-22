a2f9bf14742f315f03ca48af1f5ea0688fd9b6f5
Kyle Schaeffer <kyle@facet.ai>
2022-02-22 10:09:40 -0500

    Improve typings and add debounced hash updates

--- src/scripts/components/app.js
+++ src/scripts/components/app.js
@@ -5,57 +5,131 @@ import { GlyphsComponent } from './component.js'
 
 /**
  * @typedef Glyph
- * @property {string}  u Unicode value
- * @property {string}  h Space-separated hexadecimal values
- * @property {string}  n Glyph name
- * @property {string=} k Comma-separated glyph keyword phrases
- * @property {string=} e Space-separated HTML entity names
+ * @prop {string} u Unicode value
+ * @prop {string} h Space-separated hexadecimal values
+ * @prop {string} n Glyph name
+ * @prop {string} [k] Comma-separated glyph keyword phrases
+ * @prop {string} [e] Space-separated HTML entity names
  */
 
-customElements.define(
-  'glyphs-app',
-  class extends GlyphsComponent {
-    constructor() {
-      super({
-        glyphs: new Map(),
-        loading: false,
-        query: '',
-      })
-    }
+class App extends GlyphsComponent {
+  template = `
+    <div class="app">
+      <h1>Glyphs</h1>
+      <div class="loading" -if="loading">Loading&hellip;</div>
+      <div class="content" -ifnot="loading">
+        <input class="search" type="search" -value="query" />
+        <textarea -value="query"></textarea>
+        <div><b>Query:</b> <span -bind="query"></span></div>
+        <slot name="content"></slot>
+      </div>
+    </div>
+  `
 
-    template() {
-      return `
-        <div class="app">
-          <h1>Glyphs</h1>
-          <div class="loading" -if="loading">Loading&hellip;</div>
-          <div class="content" -ifnot="loading">
-            <input class="search" type="search" -value="query" />
-            <textarea -value="query"></textarea>
-            <div><b>Query:</b> <span -bind="query"></span></div>
-            <slot name="content"></slot>
-          </div>
-        </div>
-      `
+  style = `
+    h1 {
+      margin-block: 0;
     }
+  `
 
-    style() {
-      return `
-        h1 {
-          margin-block: 0;
-        }
-      `
-    }
+  state = {
+    /**
+     * Glyph data
+     * @type {Map<string, Glyph>}
+     */
+    glyphs: new Map(),
 
-    onMount() {
-      this.loadGlyphData()
-    }
+    /**
+     * Glyph loading state
+     * @type {boolean}
+     */
+    loading: false,
 
-    async loadGlyphData() {
-      this.setState('loading', true)
-      const data = await fetch(`../glyphs/${UNICODE_VERSION}.json`).then((res) => res.json())
-      this.state.glyphs = new Map(data)
-      this.setState('loading', false)
-      console.log(this.state)
-    }
+    /**
+     * Search query text
+     * @type {string}
+     */
+    query: '',
+
+    /**
+     * Glyph search result keys
+     * @type {string[]}
+     */
+    results: [],
+
+    /**
+     * Selected glyph key
+     * @type {?string}
+     */
+    select: null,
   }
-)
+
+  onMount() {
+    this.loadGlyphData()
+
+    this.onHashChange()
+    window.addEventListener('hashchange', () => this.onHashChange())
+  }
+
+  onUpdate() {
+    this.debouncedUpdateHash()
+  }
+
+  async loadGlyphData() {
+    this.setState('loading', true)
+    const data = await fetch(`../glyphs/${UNICODE_VERSION}.json`).then((res) => res.json())
+    this.state.glyphs = new Map(data)
+    this.setState('loading', false)
+    console.log(this.state)
+  }
+
+  updateHash() {
+    const newHash = this.encodeState()
+    if (window.location.hash === newHash) return
+    const url = new URL(window.location)
+    url.hash = newHash
+    window.history.pushState({}, window.title, url)
+  }
+
+  debouncedUpdateHash() {
+    window.clearTimeout(this.updateHashTimer)
+    this.updateHashTimer = window.setTimeout(() => this.updateHash(), 250)
+  }
+
+  onHashChange() {
+    const hashState = this.decodeState(window.location.hash)
+    if (hashState.q !== undefined) this.setState('query', hashState.q)
+    // TODO: set `select` prop from `hashState.c`
+  }
+
+  encodeState() {
+    const hashState = {
+      q: this.state.query,
+      c: this.state.select || undefined,
+    }
+
+    const hash = `#${Object.keys(hashState)
+      .filter((key) => hashState[key] !== undefined)
+      .map((key) => `${key}=${encodeURIComponent(hashState[key])}`)
+      .join('&')}`
+    return hash
+  }
+
+  decodeState(hash) {
+    const hashState = {}
+    if (!hash) return hashState
+
+    for (const param of hash.replace('#', '').split('&')) {
+      const [key, value] = param
+        .split('=')
+        .filter((c) => c !== undefined)
+        .map((c) => decodeURIComponent(c))
+      if (!key || value === undefined || !['q', 'c'].includes(key)) continue
+
+      hashState[key] = value
+    }
+    return hashState
+  }
+}
+
+customElements.define('glyphs-app', App)
--- src/scripts/components/component.js
+++ src/scripts/components/component.js
@@ -1,50 +1,77 @@
+/**
+ * Glyphs base web component class
+ * @template S
+ */
 export class GlyphsComponent extends HTMLElement {
-  constructor(initialState = {}) {
+  /**
+   * @type {Map<string, NodeListOf<HTMLElement>>}
+   */
+  #templateElements = new Map()
+
+  /**
+   * HTML template
+   */
+  template = ''
+
+  /**
+   * CSS stylesheet
+   */
+  style = ''
+
+  /**
+   * Component state
+   * @type {S}
+   */
+  state = {}
+
+  constructor() {
     super()
-    this.state = initialState
-    this.templateElements = new Map()
-
-    this._bindTemplate()
-    this.onMount()
-    this.render()
+    window.requestAnimationFrame(() => this.#mount())
   }
 
-  template() {
-    return ''
-  }
-
-  style() {
-    return ''
-  }
-
+  /**
+   * Called on component mount, just before the first render
+   */
   onMount() {}
 
+  /**
+   * Called on each state update, just after rendering
+   */
   onUpdate() {}
 
-  setState(prop, value) {
-    this.state = { ...this.state, [prop]: value }
+  /**
+   * Set component state and trigger a new render
+   * @template T
+   * @param {string} key   State prop key
+   * @param {T}      value New state prop value
+   */
+  setState(key, value) {
+    this.state = { ...this.state, [key]: value }
     this.render()
   }
 
+  /**
+   * Render the component, updating template elements as needed
+   */
   render() {
-    for (const el of this.templateElements.get('if')) {
+    for (const el of this.#templateElements.get('if')) {
       const prop = el.getAttribute('-if')
       if (prop && this.state[prop]) el.style.display = 'block'
       else el.style.display = 'none'
     }
 
-    for (const el of this.templateElements.get('ifnot')) {
+    for (const el of this.#templateElements.get('ifnot')) {
       const prop = el.getAttribute('-ifnot')
       if (prop && !this.state[prop]) el.style.display = 'block'
       else el.style.display = 'none'
     }
 
-    for (const el of this.templateElements.get('value')) {
+    for (const el of this.#templateElements.get('value')) {
       const prop = el.getAttribute('-value')
       if (prop && this.state[prop] !== undefined) el.value = this.state[prop]
     }
 
-    for (const el of this.templateElements.get('bind')) {
+    for (const el of this.#templateElements.get('bind')) {
       const prop = el.getAttribute('-bind')
       if (!prop) continue
       const value = this.state[prop]
@@ -60,29 +87,35 @@ export class GlyphsComponent extends HTMLElement {
     this.onUpdate()
   }
 
-  _bindTemplate() {
+  #mount() {
+    this.#bindTemplate()
+    this.onMount()
+    this.render()
+  }
+
+  #bindTemplate() {
     const templateId = `${this.tagName.toLowerCase()}-template`
     let template = document.getElementById(templateId)
     if (!template) {
       template = document.createElement('template')
       template.id = templateId
-      template.innerHTML = this.template()
+      template.innerHTML = this.template
       document.body.appendChild(template)
 
       const sheet = new CSSStyleSheet()
-      sheet.replaceSync(this.style())
+      sheet.replaceSync(this.style)
       document.adoptedStyleSheets = [...document.adoptedStyleSheets, sheet]
     }
 
     this.attachShadow({ mode: 'open' }).appendChild(template.content.cloneNode(true))
     this.shadowRoot.adoptedStyleSheets = document.adoptedStyleSheets
 
-    this.templateElements.set('if', this.shadowRoot.querySelectorAll('[-if]') || [])
-    this.templateElements.set('ifnot', this.shadowRoot.querySelectorAll('[-ifnot]') || [])
-    this.templateElements.set('bind', this.shadowRoot.querySelectorAll('[-bind]') || [])
-    this.templateElements.set('value', this.shadowRoot.querySelectorAll('[-value]') || [])
+    this.#templateElements.set('if', this.shadowRoot.querySelectorAll('[-if]') || [])
+    this.#templateElements.set('ifnot', this.shadowRoot.querySelectorAll('[-ifnot]') || [])
+    this.#templateElements.set('bind', this.shadowRoot.querySelectorAll('[-bind]') || [])
+    this.#templateElements.set('value', this.shadowRoot.querySelectorAll('[-value]') || [])
 
-    this.templateElements.get('value').forEach((el) => {
+    this.#templateElements.get('value').forEach((el) => {
       const prop = el.getAttribute('-value')
       if (!prop) return
       el.oninput = (e) => this.setState(prop, e.target.value)
