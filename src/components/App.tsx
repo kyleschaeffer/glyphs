e691fc621a1c69396d8560154a93f27d276fb385
Kyle Schaeffer <kyle@facet.ai>
2022-04-04 09:38:54 -0400

    Add escape key handling

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,9 +7,25 @@ import { Search } from './Search'
 
 export const App: Component = () => {
   onMount(() => {
+    const onKeyDown = (e: KeyboardEvent) => {
+      if (e.key !== 'Escape' || e.shiftKey || e.metaKey || e.ctrlKey) return
+      if (search.state.selected !== null) {
+        e.preventDefault()
+        search.setSelected(null)
+      } else if (search.state.query.length) {
+        e.preventDefault()
+        search.setQuery('')
+      }
+    }
+    window.addEventListener('keydown', onKeyDown)
+
     const onHashChange = (e: HashChangeEvent) => hydrateHash(new URL(e.newURL).hash)
     window.addEventListener('hashchange', onHashChange)
-    onCleanup(() => window.removeEventListener('hashchange', onHashChange))
+
+    onCleanup(() => {
+      window.removeEventListener('keydown', onKeyDown)
+      window.removeEventListener('hashchange', onHashChange)
+    })
   })
 
   return (
